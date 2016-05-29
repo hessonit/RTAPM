654d47243e547cd017a36bfe75675b9577f60762
Adam Laskowski <hessonit@gmail.com>
2016-05-30 00:31:53 +0200

    added objObject

---
+++ include/objObject.h
@@ -0,0 +1,7 @@
+#include <fstream>
+#include <cstring>  
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+
+using namespace std;
---
+++ src/objObject.cpp
@@ -0,0 +1,12 @@
+#include "objObject.h"
+#include "util.h"
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+
+
+
+
+
+
+
